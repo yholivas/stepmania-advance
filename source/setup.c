813c11afd3617a3c34df0afaec63a0d0036fbba7
Daniel Olivas <yholivas@gmail.com>
2019-06-11 18:41:25 -0500

    guides turned to sprites, need bg fixed

--- source/setup.c
+++ source/setup.c
@@ -6,23 +6,32 @@
 
 #include "lrarrow.h"
 #include "udarrow.h"
+#include "lrguide.h"
+#include "udguide.h"
 #include "bg.h"
+
 
 void setup_graphics()
 {
     // store arrow sprite & bg tile into first character base slot
+    // arrows take up 2x2 = 4 tiles as they are 16x16 sprites
     memcpy(&tile_mem[4][0], lrarrowTiles, lrarrowTilesLen);
     memcpy(&tile_mem[4][4], udarrowTiles, udarrowTilesLen);
+    memcpy(&tile_mem[4][8], lrguideTiles, lrguideTilesLen);
+    memcpy(&tile_mem[4][12], udguideTiles, udguideTilesLen);
+
     memcpy(&tile_mem[0][0], bgTiles, bgTilesLen);
 	memcpy(&se_mem[30][0], bgMap, bgMapLen);
 
     // store palettes into object & bg palette mem
+    // each palette entry is 16 bits, get 16 palette entries per sprite
     memcpy(pal_obj_mem, lrarrowPal, lrarrowPalLen);
+    memcpy(&pal_obj_mem[16], lrguidePal, lrguidePalLen);
     memcpy(pal_bg_mem, bgPal, bgPalLen);
 
     oam_init(obj_buffer, 128);
     REG_BG0CNT= BG_CBB(0) | BG_SBB(30) | BG_4BPP | BG_REG_32x32;
-    REG_DISPCNT= DCNT_OBJ | DCNT_OBJ_1D | DCNT_MODE0 | DCNT_BG0;
+    REG_DISPCNT= DCNT_OBJ | DCNT_OBJ_1D | DCNT_MODE0;// | DCNT_BG0;
 }
 
 void setup_sprites(OBJ_ATTR ** sprites, u32 tid, u32 pb, int * x, int * y)
--- source/stepmania_advance.c
+++ source/stepmania_advance.c
@@ -17,17 +17,17 @@ int main()
     setup_graphics();
 
     // starting sprite positions
-    //int y_guide[NUM_ARROWS] = {16, 16, 16, 16};
+    int y_guide[NUM_ARROWS] = {16, 16, 16, 16};
     int y[NUM_ARROWS] = {160, 160, 160, 160};
     int x[NUM_ARROWS] = {L_ARR_POS, D_ARR_POS, U_ARR_POS, R_ARR_POS};
 
     // sprite arrays
-    OBJ_ATTR * arrows[NUM_ARROWS];//, guide_arrows[NUM_ARROWS];
+    OBJ_ATTR * arrows[NUM_ARROWS], * guide_arrows[NUM_ARROWS];
     for (i = 0; i < NUM_ARROWS; i++) arrows[i] = &obj_buffer[i];
-    //for (i = 0; i < NUM_ARROWS; i++) guide_arrows[i] = &obj_buffer[4 + i];
+    for (i = 0; i < NUM_ARROWS; i++) guide_arrows[i] = &obj_buffer[4 + i];
 
     setup_sprites(arrows, 0, 0, x, y); // tile id, pal-bank
-    //setup_sprites(guide_arrows, 2, 1, x, y_guide); // tile id, pal-bank
+    setup_sprites(guide_arrows, 8, 1, x, y_guide); // tile id, pal-bank
 
     while (1) {
         vid_vsync();
@@ -36,7 +36,7 @@ int main()
             if (y[i] <= -16 || y[i] > 160) y[i] = 160;
         }
         for (i = 0; i < NUM_ARROWS; i++) obj_set_pos(arrows[i], x[i], y[i]);
-        oam_copy(oam_mem, obj_buffer, 4);
+        oam_copy(oam_mem, obj_buffer, 8);
     }
 
     return 0;
