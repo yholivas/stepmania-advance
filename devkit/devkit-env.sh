85623fece967fd4dc81fd5ac50a80779333b9927
Daniel Olivas <yholivas@gmail.com>
2025-01-10 11:35:49 -0600

    mac uses ccls and needs extra PATH change

--- devkit/devkit-env.sh
+++ devkit/devkit-env.sh
@@ -2,4 +2,4 @@ export DEVKITPRO=/opt/devkitpro
 export DEVKITARM={$DEVKITPRO}/devkitARM
 export DEVKITPPC={$DEVKITPRO}/devkitPPC
 
-export PATH="$PATH:$DEVKITPRO/tools/bin"
+export PATH="$PATH:$DEVKITPRO/tools/bin:$DEVKITARM/bin"
